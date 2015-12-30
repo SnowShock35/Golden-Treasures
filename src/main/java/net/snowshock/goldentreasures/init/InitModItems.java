4049a247e451ba48822cd84c81d4680998d30460
SnowShock35 <xsnowshockx35@gmail.com>
2015-12-30 13:44:53 +0000

    - ALTERED: Creative Tab Icon - OTHER: Some minor cleanups

--- src/main/java/net/snowshock/goldentreasures/init/InitModItems.java
+++ src/main/java/net/snowshock/goldentreasures/init/InitModItems.java
@@ -9,23 +9,23 @@ import net.snowshock.goldentreasures.references.ReferencesModItems;
 @GameRegistry.ObjectHolder(ReferencesModInfo.MOD_ID)
 public class InitModItems {
     public static final ItemGoldenTreasures golden_bomb = new ItemGoldenBomb();
-    public static final ItemGoldenTreasures golden_chalice = new ItemGoldenChalice();
     public static final ItemGoldenTreasures golden_coin = new ItemGoldenCoin();
-    public static final ItemGoldenTreasures golden_lantern = new ItemGoldenLantern();
-    public static final ItemGoldenTreasures golden_staff = new ItemGoldenStaff();
     public static final ItemGoldenTreasures golden_miner = new ItemGoldenMiner();
-    public static final ItemFood itemGoldenFood = null;
+    public static final ItemGoldenTreasures golden_chalice = new ItemGoldenChalice();
+    public static final ItemGoldenTreasures golden_lantern = new ItemGoldenLantern();
+    public static final ItemGoldenTreasures golden_staff = null;
+    public static final ItemFood golden_food = null;
 
 
 
     public static void init() {
         GameRegistry.registerItem(golden_bomb, ReferencesModItems.GOLDEN_BOMB);
-        GameRegistry.registerItem(golden_chalice, ReferencesModItems.GOLDEN_CHALICE);
         GameRegistry.registerItem(golden_coin, ReferencesModItems.GOLDEN_COIN);
-        GameRegistry.registerItem(golden_lantern, ReferencesModItems.GOLDEN_LANTERN);
         GameRegistry.registerItem(golden_miner, ReferencesModItems.GOLDEN_MINER);
-        GameRegistry.registerItem(golden_staff, ReferencesModItems.GOLDEN_STAFF);
+        GameRegistry.registerItem(golden_chalice, ReferencesModItems.GOLDEN_CHALICE);
+        GameRegistry.registerItem(golden_lantern, ReferencesModItems.GOLDEN_LANTERN);
         GameRegistry.registerItem(new ItemGoldenFood(), ReferencesModItems.GOLDEN_FOOD);
+        GameRegistry.registerItem(new ItemGoldenStaff(), ReferencesModItems.GOLDEN_STAFF);
     }
 }
 
