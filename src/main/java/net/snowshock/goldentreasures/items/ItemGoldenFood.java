a3384e6f2b416dbaf18855e62d69d07fe3a14aaa
SnowShock35 <xsnowshockx35@gmail.com>
2015-12-30 13:33:22 +0000

    Implemented Golden Food: Yummy!

--- src/main/java/net/snowshock/goldentreasures/items/ItemGoldenFood.java
+++ src/main/java/net/snowshock/goldentreasures/items/ItemGoldenFood.java
@@ -2,29 +2,84 @@ package net.snowshock.goldentreasures.items;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.snowshock.goldentreasures.GoldenTreasures;
+import net.snowshock.goldentreasures.references.ReferencesModInfo;
 import net.snowshock.goldentreasures.references.ReferencesModItems;
 
-public class ItemGoldenFood extends ItemGoldenTreasures {
+public class ItemGoldenFood extends ItemFood {
     public ItemGoldenFood() {
-        super();
+        super(20, 1.0F, false);
         this.setUnlocalizedName(ReferencesModItems.GOLDEN_FOOD);
+        this.setCreativeTab(GoldenTreasures.CREATIVE_TAB);
         this.setMaxStackSize(64);
         this.setMaxDamage(0);
         canRepair = false;
     }
 
     @Override
+    public String getUnlocalizedName() {
+        return String.format("item.%s%s", ReferencesModInfo.MOD_ID + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    public String getUnlocalizedName(ItemStack itemStack) {
+        return String.format("item.%s%s", ReferencesModInfo.MOD_ID + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconRegister) {
+        itemIcon = iconRegister.registerIcon(String.format("%s", getUnwrappedUnlocalizedName(this.getUnlocalizedName())));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName) {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+
+    @Override
     @SideOnly(Side.CLIENT)
     public boolean hasEffect(ItemStack stack, int pass) {
-        return false;
+        return true;
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public EnumRarity getRarity(ItemStack stack) {
-        return EnumRarity.common;
+        return EnumRarity.rare;
+    }
+
+    @Override
+    public ItemStack onEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+        --par1ItemStack.stackSize;
+        par3EntityPlayer.getFoodStats().addStats(10, 1.0F);
+        par2World.playSoundAtEntity(par3EntityPlayer, "random.burp", 0.5F, par2World.rand.nextFloat() * 0.1F + 0.9F);
+        this.onFoodEaten(par1ItemStack, par2World, par3EntityPlayer);
+        return par1ItemStack;
+    }
+
+    @Override
+    public int getMaxItemUseDuration(ItemStack par1ItemStack) {
+        return 16;
+    }
+
+    @Override
+    public EnumAction getItemUseAction(ItemStack par1ItemStack) {
+        return EnumAction.eat;
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+        if (par3EntityPlayer.canEat(false)) {
+            par3EntityPlayer.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
+        }
+
+        return par1ItemStack;
     }
 
 }
