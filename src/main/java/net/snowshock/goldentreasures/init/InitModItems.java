9bb92c448ff89703170b0ee1c94f0eb42333fa9f
Lyrael <darkangael@gmail.com>
2015-12-31 14:14:53 +1100

    Fixed up objectholder references for the rest of the items. Also manually merged Snow's re-organisation to prevent confusion.

--- src/main/java/net/snowshock/goldentreasures/init/InitModItems.java
+++ src/main/java/net/snowshock/goldentreasures/init/InitModItems.java
@@ -8,27 +8,25 @@ import net.snowshock.goldentreasures.references.ReferencesModItems;
 
 @GameRegistry.ObjectHolder(ReferencesModInfo.MOD_ID)
 public class InitModItems {
-    public static final ItemGoldenTreasures golden_bomb = new ItemGoldenBomb();
-    public static final ItemGoldenTreasures golden_coin = new ItemGoldenCoin();
-    public static final ItemGoldenTreasures golden_miner = new ItemGoldenMiner();
-    public static final ItemGoldenTreasures golden_chalice = new ItemGoldenChalice();
-    public static final ItemGoldenTreasures golden_lantern = new ItemGoldenLantern();
+    public static final ItemGoldenTreasures golden_bomb = null;
+    public static final ItemGoldenTreasures golden_coin = null;
+    public static final ItemGoldenTreasures golden_miner = null;
+    public static final ItemGoldenTreasures golden_chalice = null;
+    public static final ItemGoldenTreasures golden_lantern = null;
     public static final ItemGoldenTreasures golden_staff = null;
     public static final ItemFood golden_food = null;
 
-
-
     public static void init() {
-        GameRegistry.registerItem(golden_bomb, ReferencesModItems.GOLDEN_BOMB);
-        GameRegistry.registerItem(golden_coin, ReferencesModItems.GOLDEN_COIN);
-        GameRegistry.registerItem(golden_miner, ReferencesModItems.GOLDEN_MINER);
-        GameRegistry.registerItem(golden_chalice, ReferencesModItems.GOLDEN_CHALICE);
-        GameRegistry.registerItem(golden_lantern, ReferencesModItems.GOLDEN_LANTERN);
+        GameRegistry.registerItem(new ItemGoldenBomb(), ReferencesModItems.GOLDEN_BOMB);
+        GameRegistry.registerItem(new ItemGoldenCoin(), ReferencesModItems.GOLDEN_COIN);
+        GameRegistry.registerItem(new ItemGoldenMiner(), ReferencesModItems.GOLDEN_MINER);
+        GameRegistry.registerItem(new ItemGoldenChalice(), ReferencesModItems.GOLDEN_CHALICE);
+        GameRegistry.registerItem(new ItemGoldenLantern(), ReferencesModItems.GOLDEN_LANTERN);
         GameRegistry.registerItem(new ItemGoldenFood(), ReferencesModItems.GOLDEN_FOOD);
         GameRegistry.registerItem(new ItemGoldenStaff(), ReferencesModItems.GOLDEN_STAFF);
     }
 }
 
-//        TODO: Reimpliment these
-//        public static final ItemGoldenTreasures golden_lilypad = new ItemGoldenLilypad();
-//        GameRegistry.registerItem(golden_lilypad, ReferencesModItems.GOLDEN_LILYPAD);
+// TODO: Reimpliment these
+//    public static final ItemGoldenTreasures golden_lilypad = null;
+//    GameRegistry.registerItem(golden_lilypad, ReferencesModItems.GOLDEN_LILYPAD);
